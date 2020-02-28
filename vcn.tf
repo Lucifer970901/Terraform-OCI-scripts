3dfbe412b88159e492dfb740c47254f55737187f
Lucifer970901 <meghashreetendulkar@gmail.com>
2020-02-28 12:53:30 +0530

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -1,3 +1,4 @@
+#This file contains A vcn with public and private subnets and associated route tables and security lists for each of the subnets configured
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
@@ -18,48 +19,55 @@ data "oci_identity_availability_domains" "ads" {
 }
 #declare the resources to be deployed
 resource "oci_core_vcn" "vcn1" {
-  cidr_block     = "192.0.0.0/16"  # you can choose any other CIDR range, display name and dns label as you wish
+  cidr_block     = "192.0.0.0/16"
   dns_label      = "vcn1"
   compartment_id = "${var.compartment_ocid}"
   display_name   = "vcn1"
 }
-
 resource "oci_core_internet_gateway" "vcn1" {
 compartment_id = "${var.compartment_ocid}"
 vcn_id = "${oci_core_vcn.vcn1.id}"
 }
 
-resource "oci_core_route_table" "vcn1"{
-#route_table_id = "${oci_core_vcn_vcn1.route_table_id}"
+resource "oci_core_route_table" "publicRT"{
 compartment_id = "${var.compartment_ocid}"
 vcn_id = "${oci_core_vcn.vcn1.id}"
-
+display_name = "public_route_table"
 route_rules{
-destination = "0.0.0.0/0" # this allows all the ip addresses to pass through internet gateway, you can mention if you need any particular ranges or an IP in specific
+destination = "0.0.0.0/0"
 network_entity_id = "${oci_core_internet_gateway.vcn1.id}"
 }
 }
+resource "oci_core_route_table" "privateRT"{
+compartment_id = "${var.compartment_ocid}"
+vcn_id = "${oci_core_vcn.vcn1.id}"
+display_name = "private_route_table"
+}
+
 resource "oci_core_subnet" "publicvcn1"{
-dns_label = "publicVcn1" # you can choose any other CIDR range, display name and dns label as you wish
+dns_label = "publicVcn1"
 compartment_id = "${var.compartment_ocid}"
 vcn_id = "${oci_core_vcn.vcn1.id}"
 display_name = "public_subnet_vcn1"
-cidr_block = "192.0.0.0/24" 
-#availability_domain = "${data.oci_ientity_availability_domains.ads.availability_domain}"
+cidr_block = "192.0.0.0/24"
+route_table_id = "${oci_core_route_table.publicRT.id}"
+security_list_ids = ["${oci_core_security_list.publicSL.id}"]
 }
 
 resource "oci_core_subnet" "privatevcn1"{
-dns_label = "privateVcn1"
+dns_label = "privatevcn1"
 compartment_id = "${var.compartment_ocid}"
 vcn_id = "${oci_core_vcn.vcn1.id}"
 display_name = "private_subnet_vcn1"
 cidr_block = "192.0.1.0/24"
 prohibit_public_ip_on_vnic = "true"
+route_table_id = "${oci_core_route_table.privateRT.id}"
+security_list_ids = ["${oci_core_security_list.privateSL.id}"]
 }
-resource "oci_core_security_list" "this" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_vcn.this.id
-  display_name   = "webserver_security_list"
+resource "oci_core_security_list" "publicSL" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${oci_core_vcn.vcn1.id}"
+  display_name   = "public_security_list"
 
   egress_security_rules {
     protocol    = "all"
@@ -67,26 +75,8 @@ resource "oci_core_security_list" "this" {
   }
   ingress_security_rules {
     tcp_options {
-      
       max = "22"
       min = "22"
-    }
-
-    protocol = "6"
-    source   = "0.0.0.0/0"
-  }
-{{  ingress_security_rules {
-    tcp_options {
-      max = "80"
-      min = "80"
-    }
-    protocol = "6"
-    source   = "0.0.0.0/0"
-  }
-  ingress_security_rules {
-    tcp_options {
-      max = "443"
-      min = "443"
     }
 
     protocol = "6"
@@ -118,8 +108,52 @@ resource "oci_core_security_list" "this" {
     source   = "0.0.0.0/0"
   }
 }
-#output the results
+resource "oci_core_security_list" "privateSL" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${oci_core_vcn.vcn1.id}"
+  display_name   = "private_security_list"
 
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+
+    protocol = "6"
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = "0"
+    }
+
+    protocol = "1"
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = "3"
+      code = "4"
+    }
+
+    protocol = "1"
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+ icmp_options {
+      type = "8"
+    }
+
+    protocol = "1"
+    source   = "0.0.0.0/0"
+  }
+}
+
+#output the results
 output "vcn_id" {
   value = "${oci_core_vcn.vcn1.id}"
 }
@@ -129,12 +163,27 @@ value = "${oci_core_internet_gateway.vcn1.id}"
 output "show-ads" {
   value = "${data.oci_identity_availability_domains.ads.availability_domains}"
 }
-output "default_route_table_id"{
-value = "${oci_core_route_table.vcn1.id}"
+output "public_route_table_id"{
+value = "${oci_core_route_table.publicRT.id}"
 }
+output "private_route_table_id"{
+value = "${oci_core_route_table.privateRT.id}"
+}
+
 output "public_subnet_id"{
 value = "${oci_core_subnet.publicvcn1.id}"
 }
 output "private_subnet_id"{
 value = "${oci_core_subnet.privatevcn1.id}"
 }
+output"public_security_list_id"{
+value = "${oci_core_security_list.publicSL.id}"
+}
+output"private_security_list_id"{
+value = "${oci_core_security_list.privateSL.id}"
+}
+
+
+
+
+  
