2600eca60e8f28dcf3e7fc2c056f92b2a08f34f8
Lucifer970901 <meghashreetendulkar@gmail.com>
2020-03-02 11:26:19 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,2 @@
+# This is the configuaration file for creating user, groups and related policies to create the users and give access to them globally.
+#this configuaration file creates users in your tenancy and providesthe user a resource in Oracle Cloud Inrafrasturcture Identity services. 
